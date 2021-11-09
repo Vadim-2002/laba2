3d576462ee36cb8569d890b6cd1ef5436f28ec6a
Vadim-2002 <vadim.winter2@mail.ru>
2021-11-09 19:35:31 +0500

    Создал класс Эллипс с тримя конструкторами (конструктор по умолчанию, конструктор с параметрами и конструктор копирования) и диструктором.

---
+++ work.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdlib>
+
+class Ellipse
+{
+    protected:
+        int x, y;
+        int a, b;
+
+    public:
+        Ellipse()
+        {
+            printf("\nEllipse()\n");
+
+            x = 0, y = 0;
+            a = 0, b = 0;
+        }
+
+        Ellipse(int x, int y, int a, int b)
+        {
+            printf("\nEllipse(int x, int y, int a, int b)\n");
+
+            this->x = x;
+            this->y = y;
+            this->a = a;
+            this->b = b;
+        }
+
+        Ellipse(Ellipse &object)
+        {
+            printf("\nEllipse(Ellipse &object)\n");
+
+            x = object.x;
+            y = object.y;
+            a = object.a;
+            b = object.b;
+        }
+
+        ~Ellipse()
+        {
+            printf("\nx = %d, y = %d, a = %d, b = %d\n", x, y, a, b);
+            printf("\n~Ellipse()\n");
+        }
+};
+
+int main()
+{
+    {
+    Ellipse ellipse;
+    Ellipse ellipse2(5, 5, 20, 10);
+    Ellipse ellipse3(ellipse2);
+    }
+
+    system("pause");
+    
+    return 0;
+}
